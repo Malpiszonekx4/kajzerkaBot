1118b94152bbecec7353add0bccef07c97442bb4
Malpiszonekx4 <55206564+Malpiszonekx4@users.noreply.github.com>
2021-02-13 21:19:38 +0100

    fix

--- events/createVcJoin.js
+++ events/createVcJoin.js
@@ -12,9 +12,10 @@ async function event(bot){
     if(bot == undefined) return console.trace("No client specified")
     bot.on("voiceStateUpdate", async (oldState, newState)=>{
         let createVcChannel = await getCreateVcChannelId(newState.guild.id)
-        if(newState.channelID != createVcChannel.channelId) return
-        if(!newState.guild.me.hasPermission("MOVE_MEMBERS")) return
-        if(!newState.guild.me.hasPermission("MANAGE_CHANNELS")) return
+        if(createVcChannel == undefined) return; // guild didn't setup channel
+        if(newState.channelID != createVcChannel.channelId) return // wrong channel
+        if(!newState.guild.me.hasPermission("MOVE_MEMBERS")) return // bot doesn't have required permissions
+        if(!newState.guild.me.hasPermission("MANAGE_CHANNELS")) return // bot doesn't have required permissions
         let name = await getCreateVcChannelName(newState.guild.id)
         if(name == undefined) name = `🔈 ${newState.member.displayName}`
         name = name.replace("%usernick%", newState.member.displayName)
