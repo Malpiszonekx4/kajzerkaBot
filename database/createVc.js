a4ebb72337781e48d2c45ce251480496126e5f41
Malpiszonekx4 <55206564+Malpiszonekx4@users.noreply.github.com>
2021-03-16 21:18:38 +0100

    update createVc database

--- database/createVc.js
+++ database/createVc.js
@@ -4,14 +4,14 @@ const db = new Datastore({filename: "./database/createVc.db", autoload: true});
 exports.setCreateVcChannelId = (channelId, guildId)=>{
     db.findOne({guildId: guildId}, (err, doc)=>{
         if(err) return console.error(err)
-        if(doc == null) db.insert({guildId: guildId, channelId: channelId, rowType:"createVcChannelId"})
-        else db.update({guildId: guildId, rowType:"createVcChannelId"}, {$set: {channelId: channelId}})
+        if(doc == null) db.insert({guildId: guildId, channelId: channelId})
+        else db.update({guildId: guildId}, {$set: {channelId: channelId}})
     })
 }
 
 exports.getCreateVcChannelId = (guildId)=>{
     return new Promise(Resolve =>{
-        db.findOne({guildId: guildId, rowType:"createVcChannelId"}, (err, doc)=>{
+        db.findOne({guildId: guildId}, (err, doc)=>{
             if(err) return console.error(err)
             if(doc == null) Resolve(undefined)
             else Resolve(doc)
@@ -20,16 +20,16 @@ exports.getCreateVcChannelId = (guildId)=>{
 }
 
 exports.setCreateVcChannelName = (name, guildId)=>{
-    db.findOne({guildId: guildId, rowType:"createVcChannelName"}, (err, doc)=>{
+    db.findOne({guildId: guildId}, (err, doc)=>{
         if(err) return console.error(err)
-        if(doc == null) db.insert({guildId: guildId, name: name, rowType:"createVcChannelName"})
-        else db.update({guildId: guildId, rowType:"createVcChannelId"}, {$set: {name: name}})
+        if(doc == null) db.insert({guildId: guildId, name: name})
+        else db.update({guildId: guildId}, {$set: {name: name}})
     })
 }
 
 exports.getCreateVcChannelName = (guildId)=>{
     return new Promise(Resolve =>{
-        db.findOne({guildId: guildId, rowType:"createVcChannelName"}, (err, doc)=>{
+        db.findOne({guildId: guildId}, (err, doc)=>{
             if(err) return console.error(err)
             if(doc == null) Resolve(undefined)
             else Resolve(doc)
