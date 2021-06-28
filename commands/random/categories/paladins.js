58b1f116b3eef2167f35d6de7fb423da85e60ac1
Malpiszonekx4 <55206564+Malpiszonekx4@users.noreply.github.com>
2021-06-28 17:39:11 +0200

    add League of Legends champions, Paladins gamemodes and maps categories to random cmd

--- commands/random/categories/paladins.js
+++ commands/random/categories/paladins.js
@@ -1,4 +1,4 @@
-module.exports.damages = [
+module.exports.champions.damages = [
 	"Bomb King",
 	"Cassie",
 	"Dredge",
@@ -16,7 +16,7 @@ module.exports.damages = [
 	"Willo"
 ]
 
-module.exports.tanks = [
+module.exports.champions.tanks = [
 	"Ash",	
 	"Atlas",
 	"Barik",
@@ -31,7 +31,7 @@ module.exports.tanks = [
 	"Yagorath"
 ]
 
-module.exports.healers = [
+module.exports.champions.healers = [
 
 	"Corvus",
 	"Furia",
@@ -45,7 +45,7 @@ module.exports.healers = [
 	"Ying"
 ]
 
-module.exports.flanks = [
+module.exports.chamions.flanks = [
 	"Vora",
 	"Buck",
 	"Evie",
@@ -59,9 +59,51 @@ module.exports.flanks = [
 	"Vatu"
 ]
 
-module.exports.all = new Array().concat(module.exports.damages).concat(module.exports.tanks).concat(module.exports.healers).concat(module.exports.flanks)
+module.exports.champions.all = new Array().concat(module.exports.damages).concat(module.exports.tanks).concat(module.exports.healers).concat(module.exports.flanks)
 
-module.exports.notanks = new Array().concat(module.exports.damages).concat(module.exports.healers).concat(module.exports.flanks)
-module.exports.nohealers = new Array().concat(module.exports.damages).concat(module.exports.healers).concat(module.exports.flanks)
-module.exports.noflanks = new Array().concat(module.exports.damages).concat(module.exports.healers).concat(module.exports.flanks)
-module.exports.nodamages = new Array().concat(module.exports.damages).concat(module.exports.healers).concat(module.exports.flanks)
+module.exports.champions.notanks = new Array().concat(module.exports.damages).concat(module.exports.healers).concat(module.exports.flanks)
+module.exports.champions.nohealers = new Array().concat(module.exports.damages).concat(module.exports.healers).concat(module.exports.flanks)
+module.exports.champions.noflanks = new Array().concat(module.exports.damages).concat(module.exports.healers).concat(module.exports.flanks)
+module.exports.champions.nodamages = new Array().concat(module.exports.damages).concat(module.exports.healers).concat(module.exports.flanks)
+
+module.exports.gamemodes = [
+	"Siege",
+	"Team DM",
+	"King of the Hill",
+	"Onslaught"
+]
+module.exports.maps.siege = [
+	"Frog Isle",
+	"Jaguar Falls",
+	"Serpen Beach",
+	"Frozen Guard",
+	"Ice Mines",
+	"Fish Market",
+	"Timber Mill",
+	"Stone Keep",
+	"Brightmarsh",
+	"Splitstone Quarry",
+	"Ascension Peak",
+	"Warder's Gate",
+	"Shattered Desert",
+	"Bazaar"
+]
+module.exports.maps.onslaught = [
+	"Primal Court",
+	"Foreman's Rise",
+	"Magistrate's Archives",
+	"Marauder's Port"
+]
+module.exports.maps.koth = [
+	"Snowfall Junction",
+	"Magistrate's Archives",
+	"Trade District",
+	"Marauder's Port"
+]
+module.exports.maps.tdm = [
+	"Snowfall Junction",
+	"Trade District",
+	"Abbys",
+	"Throne",
+	"Dragon Arena"
+]
